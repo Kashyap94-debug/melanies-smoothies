48ef7de9dbe75e427969c25c63995e8d6180aa57
Kashyap94-debug <nehakashyapk94@gmail.com>
2026-04-17 15:49:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 # Import python packages
 import streamlit as st
-import pandas as pd
-import requests
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -29,7 +27,6 @@ option = st.selectbox(
 st.write('your favourite is:', option)
 
 from snowflake.snowpark.functions import col
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -46,16 +43,10 @@ ingredients_list =st.multiselect(
 ingredients_string=""
 
 if ingredients_list:
-    ingredients_string=''
+    
+
     for fruit_chosen in ingredients_list:
-        ingredients_string+=fruit_chosen+' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen+'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)  
-        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True) 
-
-
+        ingredients_string += fruit_chosen+' '
 
 # st.write(ingredients_string)
 
