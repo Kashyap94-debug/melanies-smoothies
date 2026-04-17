c8786f5864b653b013c1e922980449f090f4bcc3
Kashyap94-debug <nehakashyapk94@gmail.com>
2026-04-17 15:52:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,4 +67,9 @@ time_to_insert =st.button('Submit order')
 
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")    
+    st.success('Your Smoothie is ordered!', icon="✅")  
+
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
